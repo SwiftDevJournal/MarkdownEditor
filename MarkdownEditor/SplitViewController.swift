0ccd49ca43612bd0a9a4a2d96ea2888a22375b17
Mark Szymczyk <mark@meandmark.com>
2020-02-14 15:42:38 -0500

    Add a file for the split view controller.

---
+++ MarkdownEditor/SplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplitViewController.swift
+//  MarkdownEditor
+//
+//  Created by mark on 2/14/20.
+//  Copyright © 2020 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
