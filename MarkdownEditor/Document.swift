559763dc688816ec22414150dca5bf517cddc6c7
Mark Szymczyk <mark@meandmark.com>
2020-02-14 16:12:53 -0500

    Add code to save and load the document data.

--- MarkdownEditor/Document.swift
+++ MarkdownEditor/Document.swift
@@ -30,14 +30,18 @@ class Document: NSDocument {
     override func data(ofType typeName: String) throws -> Data {
         // Insert code here to write your document to data of the specified type, throwing an error in case of failure.
         // Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        
+        return markdown.data(using: .utf8) ?? Data()
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
         // Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
         // Alternatively, you could remove this method and override read(from:ofType:) instead.
         // If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        
+        if let fileContents = String(data: data,encoding: .utf8) {
+            markdown = fileContents
+        }
     }
 
 
