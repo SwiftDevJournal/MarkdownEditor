e1457383d360dea4b5278061021f027433810fa6
Mark Szymczyk <mark@meandmark.com>
2020-02-14 15:41:57 -0500

    Add a file for the preview view controller.

---
+++ MarkdownEditor/PreviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewViewController.swift
+//  MarkdownEditor
+//
+//  Created by mark on 2/14/20.
+//  Copyright © 2020 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class PreviewViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
