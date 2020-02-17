4de25db610bfbbce253d89002077f7a0421c7b96
Mark Szymczyk <mark@meandmark.com>
2020-02-17 15:20:40 -0500

    Add actions to create the tags from the Markdown menu.

--- MarkdownEditor/TagCreation.swift
+++ MarkdownEditor/TagCreation.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2020 Swift Dev Journal. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+extension TextViewController {
+    @IBAction func addTag(_ sender: NSMenuItem) {
+        // TODO: Find a better way to associate a tag with a menu item that doesn't rely on the menu item text, which would break if I were to translate to other languages. I would also like to find a way that works with toolbar buttons for common tags.
+        // Is there a way to associate a menu item with the TagName Swift enum? If I could get that to work, it would also work with toolbar buttons.
+        let tagTitle = sender.title
+        createTag(name: tagTitle)
+        
+        NotificationCenter.default.post(name: NSText.didChangeNotification, object: textView)
+    }
+    
+    func createTag(name: String) {
+        let selection = textView?.selectedRange
+        
+        let tag = Tag(title: name)
+        let prefix = tag.getMarkupPrefix()
+        let suffix = tag.getMarkupSuffix()
+        
+        let prefixAttributedString = NSAttributedString(string: prefix)
+        textView.textStorage?.insert(prefixAttributedString, at: (selection?
+            .location)!)
+        
+        let suffixAttributedString = NSAttributedString(string: suffix)
+        let insertionPoint = (selection?.location)! + (selection?.length)! + prefix.count
+        textView.textStorage?.insert(suffixAttributedString, at: insertionPoint)
+    }
+    
+}
